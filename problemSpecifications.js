3fab77bad52c2eebb79fbd9773a28901c62e442d
Monica Verdi <monica.verdi@gmail.com>
2021-01-27 14:35:36 +0100

    add raindrop function

--- problemSpecifications.js
+++ problemSpecifications.js
@@ -71,3 +71,24 @@ function isLeapYear(year); {
 
 isLeapYear(1900)
 
+//RAINDROP
+
+/*The rules of raindrops are that if a given number:
+
+has 3 as a factor, add 'Pling' to the result.
+has 5 as a factor, add 'Plang' to the result.
+has 7 as a factor, add 'Plong' to the result.
+does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.*/
+
+function raindrops(number) {
+  if (number %3 === 0) {
+    number = "Pling"
+  } else if (number %5 === 0) {
+    number = "Plang"
+  } else if (number %7 === 0) {
+    number = "Plong"
+  } else {
+    return number
+  }
+  console.log(number)
+} 
